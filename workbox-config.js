43689a2148dbc7d1c836822b4180bc3a0a602ac9
nd-02110114 <nd.12021218@gmail.com>
2018-05-14 23:16:33 +0900

    feat: integrate service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    globDirectory: "_book",
     globPatterns: [
         "**/*.{js,css,html,png,eot,svg,ttf,woff,woff2,otf,,ico}"
     ],
     swSrc: "source/sw.js",
-    swDest: "_book/sw.js",
 };
