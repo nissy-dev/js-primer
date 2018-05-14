9fc0093cfba2833af6801f3f42acb8dbbb8bb4b8
nd-02110114 <nd.12021218@gmail.com>
2018-05-14 23:33:03 +0900

    fix: workbox configuration

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     globPatterns: [
-        "**/*.{js,css,html,png,eot,svg,ttf,woff,woff2,otf,,ico}"
+        "**/*.{js,css,html,png,eot,svg,ttf,woff,woff2,otf,ico}"
     ],
     swSrc: "source/sw.js",
 };
